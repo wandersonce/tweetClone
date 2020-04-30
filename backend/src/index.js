21f53b61f8446462abfeda5d7d19af0b7f7bf7f2
Wanderson Castro <wandersonce1@hotmail.com>
2020-04-29 22:02:00 -0700

    backend Done

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+mongoose.connect('mongodb+srv://wanderson:f1a6r2a7@twittercloneapp-frhue.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(require('./routes'));
+
+server.listen(3000, () => {
+    console.log('Server Started on port 3000');
+});
