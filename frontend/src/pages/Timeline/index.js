060ca6f84902b87aaf1cf69916ebdb3e319fc5b8
Wanderson Castro <wandersonce1@hotmail.com>
2020-05-01 21:53:20 -0700

    timeline created

--- frontend/src/pages/Timeline/index.js
+++ frontend/src/pages/Timeline/index.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+
+import twitterLogo from '../../twitter.svg';
 import './styles.css';
 
 export default class Timeline extends Component {
+    state = {
+        newTweet: '',
+    };
+
+    handleInputChange = (e) => {
+        this.setState({ newTweet: e.target.value });
+    };
+
+    handleNewTweet = (e) => {
+        if (e.keyCode != 13) return;
+
+        const content = this.state.newTweet;
+        const author = localStorage.getItem("@GoTwitter:username")
+        console.log(content);
+
+    };
+
     render() {
         return (
-            <h1>Timeline!</h1>
+            <div className="timeline-wrapper">
+                <img height={24} src={twitterLogo} alt="GoTwitter" />
+
+                <form>
+                    <textarea
+                        value={this.state.newTweet}
+                        onChange={this.handleInputChange}
+                        onKeyDown={this.handleNewTweet}
+                        placeholder={"What is Happening " + localStorage.getItem("@GoTwitter:username") + "?"}
+                    />
+                </form>
+            </div>
         )
     }
 }
