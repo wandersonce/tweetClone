c5188cb73e657c3123c3dce307452c0c02119aea
Wanderson Castro <wandersonce1@hotmail.com>
2020-05-01 21:41:51 -0700

    login function created

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -4,12 +4,36 @@ import twitterLogo from '../../twitter.svg';
 import './styles.css'
 
 export default class Login extends Component {
+    state = {
+        username: ""
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const { username } = this.state;
+
+        if (!username.length) return;
+
+        localStorage.setItem('@GoTwitter:username', username);
+
+        this.props.history.push('/timeline');
+    };
+
+    handleInputChange = (e) => {
+        this.setState({ username: e.target.value });
+    };
+
     render() {
         return (
             <div className="login-wrapper">
                 <img src={twitterLogo} alt="GoTwitter" />
-                <form>
-                    <input placeholder="Username" />
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        value={this.state.username}
+                        onChange={this.handleInputChange}
+                        placeholder="Username"
+                    />
                     <button type="submit">Enter</button>
                 </form>
             </div>
